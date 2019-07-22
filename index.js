a73c96e87721456086bfbf8a901c159811365382
Rupinder <rupinderdhariwal@gmail.com>
2019-07-21 23:40:32 -0400

    added prettier to pre-commit with some pre linted code

---
+++ index.js
@@ -0,0 +1,9 @@
+const TEST = '123'
+        const TEST1 = "123";
+// this is atest
+
+/* this is another test */
+        console.log(TEST)
+
+function test()  {   console.log("test")}
+// this is a really long line of comments, and this should trip up the linter
